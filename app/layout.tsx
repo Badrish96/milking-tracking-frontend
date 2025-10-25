01bb6fee4f31ed70ad40059e696a0d6d27293548
Badrish Choubey <badrish@onedao.me>
2025-10-25 19:07:42 +0530

    added vercel insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from 'next';
 import { Inter } from 'next/font/google';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -23,6 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en" data-scroll-behavior="smooth">
       <body className={inter.className}>
+        <SpeedInsights/>
         {children}
       </body>
     </html>
